d3efe4b6049daf338d70dac092f75fc163dc583c
vandoan <vandoona@gmail.com>
2017-08-31 12:19:04 -0700

    class testing and buildout

---
+++ FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,43 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Van Doan on 8/31/17.
+//  Copyright © 2017 Coco Bay. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+    
+    //MARK: Meal Class Tests
+    
+    // Confirm that the Meal initializer returns a Meal object when passed valadid parameters.
+    func testMealInitializationSucceeds(){
+        
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest postive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+
+    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails(){
+       
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+        
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+    }
+    
+}
+
